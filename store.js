982b891772d476b0824ec288ad401f47609ce2a3
AssaadZiadeh <102186508+AssaadZiadeh@users.noreply.github.com>
2022-04-18 22:28:41 +0300

    Update store.js

--- store.js
+++ store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './ReduxSlices/AuthSlice.js';
+import stockReducer from './ReduxSlices/StocksSlice.js';
+import basketReducer from './ReduxSlices/BasketSlice.js';
+import saleReducer from './ReduxSlices/SaleSlice.js';
+import FriendReducer from './ReduxSlices/FriendSlice.js';
+import ChartReducer from './ReduxSlices/ChartSlice.js';
 
 export default configureStore({
   reducer: {
-      authentication: authReducer
+      authentication: authReducer,
+      quotes: stockReducer,
+      purchase: basketReducer,
+      sale: saleReducer,
+      friend: FriendReducer,
+      chart: ChartReducer
   },
 })
